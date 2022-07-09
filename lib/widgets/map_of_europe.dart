60b078ced1f7d43f4314a8655dd13b45a6c75f1d
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2022-07-09 10:59:00 +0200

    Update Dependencies

--- lib/widgets/map_of_europe.dart
+++ lib/widgets/map_of_europe.dart
@@ -25,15 +25,20 @@ class MapOfEurope extends StatelessWidget {
               MyColors.canvasColor,
               BlendMode.saturation,
             ),
-            child: Map(
+            child: MapLayoutBuilder(
               controller: _controller,
-              builder: (_, x, y, z) {
-                final locale = context.l10n.currentLocale;
-                final url =
-                    'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m2!1e0!2sh!3i420120484!3m1!2s$locale!5e1105!12m1!1e68!2m2!1sset!2sRoadmap!4e0!5m0!1e0!23i8289918';
+              builder: (_, __) => TileLayer(
+                builder: (_, x, y, z) {
+                  final locale = context.l10n.currentLocale;
+                  final url =
+                      'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m2!1e0!2sh!3i420120484!3m1!2s$locale!5e1105!12m1!1e68!2m2!1sset!2sRoadmap!4e0!5m0!1e0!23i8289918';
 
-                return CachedNetworkImage(imageUrl: url, fit: BoxFit.fill);
-              },
+                  return CachedNetworkImage(
+                    imageUrl: url,
+                    fit: BoxFit.fill,
+                  );
+                },
+              ),
             ),
           ),
         ),
