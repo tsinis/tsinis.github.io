7e6254554335ea61a35454e13da0e4c6846778b8
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2023-05-23 22:05:30 +0200

    fix(ci): hot-fix of beta deploys

--- lib/extensions/context_extensions.dart
+++ lib/extensions/context_extensions.dart
@@ -4,7 +4,8 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension ContextExtensions on BuildContext {
-  AppLocalizations get l10n => AppLocalizations.of(this);
+  AppLocalizations get l10n =>
+      AppLocalizations.of(this)!; // ignore: avoid-non-null-assertion
   MediaQueryData get media => MediaQuery.of(this);
   Size get screenSize => media.size;
 }
