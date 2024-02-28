3186c75fad857c635e4f7e5d90ea366794692ea3
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2024-02-28 08:31:33 +0100

    chore(deps): update ci deps and fix failing workflows

--- lib/extensions/context_extensions.dart
+++ lib/extensions/context_extensions.dart
@@ -4,8 +4,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 extension ContextExtensions on BuildContext {
-  // ignore: avoid-non-null-assertion
-  AppLocalizations get l10n => AppLocalizations.of(this)!;
+  AppLocalizations get l10n => AppLocalizations.of(this);
   MediaQueryData get media => MediaQuery.of(this);
   Size get screenSize => media.size;
 }
